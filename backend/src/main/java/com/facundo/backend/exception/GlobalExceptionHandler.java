c5b151cde23c3e4d09d246a9b957f7fa95b3eaa1
FacuAlvarezP <facutialvarez2005@gmail.com>
2026-04-10 09:25:11 -0300

    refactor: GlobalExceptionHandler with new ErrorResponse

--- backend/src/main/java/com/facundo/backend/exception/GlobalExceptionHandler.java
+++ backend/src/main/java/com/facundo/backend/exception/GlobalExceptionHandler.java
@@ -5,21 +5,40 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
+import jakarta.servlet.http.HttpServletRequest;
+
 @RestControllerAdvice
 public class GlobalExceptionHandler {
     
     @ExceptionHandler(UserAlreadyExistsException.class)
-    public ResponseEntity<String> handlerUserAlreadyExists(UserAlreadyExistsException ex) {
-        return new ResponseEntity<>(ex.getMessage(), HttpStatus.BAD_REQUEST);
+    public ResponseEntity<ErrorResponse> handlerUserAlreadyExists(UserAlreadyExistsException ex, HttpServletRequest request) {
+        ErrorResponse error = new ErrorResponse( HttpStatus.BAD_REQUEST.value(),
+        "Bad Request", ex.getMessage(), request.getRequestURI());
+
+        return new ResponseEntity<>(error, HttpStatus.BAD_REQUEST);
     }
 
     @ExceptionHandler(InvalidCredentialsException.class)
-    public ResponseEntity<String> handleInvalidCredentials(InvalidCredentialsException ex) {
-        return new ResponseEntity<>(ex.getMessage(), HttpStatus.UNAUTHORIZED);
+    public ResponseEntity<ErrorResponse> handleInvalidCredentials(InvalidCredentialsException ex, HttpServletRequest request) {
+        ErrorResponse error = new ErrorResponse(HttpStatus.UNAUTHORIZED.value(),
+        "Unauthorized", ex.getMessage(),request.getRequestURI());
+
+        return new ResponseEntity<>(error, HttpStatus.UNAUTHORIZED);
     }
 
     @ExceptionHandler(UserNotFoundException.class)
-    public ResponseEntity<String> handleUserNotFound(UserNotFoundException ex) {
-        return new ResponseEntity<>(ex.getMessage(), HttpStatus.NOT_FOUND);
+    public ResponseEntity<ErrorResponse> handleUserNotFound(UserNotFoundException ex, HttpServletRequest request) {
+        ErrorResponse error = new ErrorResponse(HttpStatus.NOT_FOUND.value(),
+        "Not Found", ex.getMessage(), request.getRequestURI());
+
+        return new ResponseEntity<>(error, HttpStatus.NOT_FOUND);
     }
+
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<ErrorResponse> handleGeneralException(Exception ex, HttpServletRequest request) {
+    ErrorResponse error = new ErrorResponse(HttpStatus.INTERNAL_SERVER_ERROR.value(),
+    "Internal Server Error", ex.getMessage(), request.getRequestURI());
+
+    return new ResponseEntity<>(error, HttpStatus.INTERNAL_SERVER_ERROR);
+}
 }
